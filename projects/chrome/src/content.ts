e274dfffdb8abccc4fa0eb8ce494e161643f5431
Jim <jim@koding.work>
2022-12-21 14:45:13 +0800

    fix: screen will flashing when user disable jira in black
    1. let content script run at document_start
    2. rename black style name in css
    3. run `initPanelNgApp` at `DOMContentLoaded`

--- projects/chrome/src/content.ts
+++ projects/chrome/src/content.ts
@@ -2,29 +2,31 @@ import * as $ from 'jquery';
 
 function initBlackTheme() {
   chrome.storage.sync.get('jiraInBlack', (cfg) => {
-    if (!cfg.jiraInBlack) {
-      $('html').attr('jira-not-in-black', '');
+    if (cfg.jiraInBlack) {
+      $('html').attr('jira-in-black', '');
     }
   });
 }
 
 function initPanelNgApp() {
-  // set data for angular panel app
-  $(
-    `<jira-killer-id id="jiraKillerId" data-runtime-id="${
-      chrome.runtime.id
-    }" data-asset-root-url="${chrome.runtime.getURL('content-panel/assets/')}"></jira-killer-id>`,
-  ).appendTo(document.body);
+  window.addEventListener('DOMContentLoaded', () => {
+    // set data for angular panel app
+    $(
+      `<jira-killer-id id="jiraKillerId" data-runtime-id="${
+        chrome.runtime.id
+      }" data-asset-root-url="${chrome.runtime.getURL('content-panel/assets/')}"></jira-killer-id>`,
+    ).appendTo(document.body);
 
-  // prepare source link of angular panel app
-  const runtime = chrome.runtime.getURL('content-panel/runtime.js');
-  const polyfills = chrome.runtime.getURL('content-panel/polyfills.js');
-  const vendor = chrome.runtime.getURL('content-panel/vendor.js');
-  const main = chrome.runtime.getURL('content-panel/main.js');
+    // prepare source link of angular panel app
+    const runtime = chrome.runtime.getURL('content-panel/runtime.js');
+    const polyfills = chrome.runtime.getURL('content-panel/polyfills.js');
+    const vendor = chrome.runtime.getURL('content-panel/vendor.js');
+    const main = chrome.runtime.getURL('content-panel/main.js');
 
-  let ngSrcHtml = '';
-  [runtime, polyfills, vendor, main].forEach((url) => (ngSrcHtml += `<script src="${url}" type="module"></script>`));
-  $(ngSrcHtml).appendTo(document.head);
+    let ngSrcHtml = '';
+    [runtime, polyfills, vendor, main].forEach((url) => (ngSrcHtml += `<script src="${url}" type="module"></script>`));
+    $(ngSrcHtml).appendTo(document.head);
+  });
 }
 
 function initMessageListener() {
