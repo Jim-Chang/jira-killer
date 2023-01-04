676020b6ac57a532a12567041510de4b681054ea
Jim <jim@koding.work>
2023-01-04 20:03:48 +0800

    fix: don't use `find` because it should fetch all sheet data
    replace to find sprint id at specific column

--- projects/dashboard-gsheet-helper/gsheet_helper.py
+++ projects/dashboard-gsheet-helper/gsheet_helper.py
@@ -18,17 +18,19 @@ class GSheetHelper:
         self.sheet = self.client.open_by_key(get_dashboard_gsheet_key())
 
     def is_sprint_exist(self, sprint_id: str):
-        col_idx = TAB_COL_IDX_MAP[Tab.SpConfig]["sprint_id"]
+        sp_col_idx = TAB_COL_IDX_MAP[Tab.SpConfig]["sprint_id"]
         wks = self.sheet.worksheet_by_title(Tab.SpConfig.value)
-        return len(wks.find(sprint_id, cols=(col_idx, col_idx))) > 0
+        sprint_id_cols = wks.get_col(sp_col_idx, include_tailing_empty=False)
+        return sprint_id in sprint_id_cols
 
     def find_user_points_by_sprint(self, sprint_id: str):
         sp_col_idx = TAB_COL_IDX_MAP[Tab.UserSpConfig]["sprint_id"]
         wks = self.sheet.worksheet_by_title(Tab.UserSpConfig.value)
-        cells = wks.find(sprint_id, cols=(sp_col_idx, sp_col_idx))
+        sprint_id_cols = wks.get_col(sp_col_idx, include_tailing_empty=False)
 
-        row_idx_list = sorted([c.row for c in cells])
-        min_row_idx, max_row_idx = row_idx_list[0], row_idx_list[-1]
+        min_row_idx = sprint_id_cols.index(sprint_id) + 1
+        sprint_id_cols.reverse()
+        max_row_idx = len(sprint_id_cols) - sprint_id_cols.index(sprint_id)
 
         uname_col_idx = TAB_COL_IDX_MAP[Tab.UserSpConfig]["jira_user_name"]
         cp_sp_col_idx = TAB_COL_IDX_MAP[Tab.UserSpConfig]["tg_cp_sp_scrum"]
@@ -36,9 +38,11 @@ class GSheetHelper:
         rows = wks.get_values(
             (min_row_idx, uname_col_idx), (max_row_idx, cp_sp_col_idx)
         )
-        # user_name_id_map = self.get_user_name_id_map()
-        # ret = {user_name_id_map.get(r[uname_col_idx - 1]): r[cp_sp_col_idx - 1] for r in rows}
-        ret = {r[uname_col_idx - 1]: r[cp_sp_col_idx - 1] for r in rows}
+        user_name_id_map = self.get_user_name_id_map()
+        ret = {
+            user_name_id_map.get(r[uname_col_idx - 1]): r[cp_sp_col_idx - 1]
+            for r in rows
+        }
         return ret
 
     def get_user_name_id_map(self):
