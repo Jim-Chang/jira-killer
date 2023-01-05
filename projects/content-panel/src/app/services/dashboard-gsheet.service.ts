d4436a610da6272b70c847ab74b4e541e79300ed
Jim <jim@koding.work>
2023-01-05 14:59:48 +0800

    feat: add budget calculate in workload panel

--- projects/content-panel/src/app/services/dashboard-gsheet.service.ts
+++ projects/content-panel/src/app/services/dashboard-gsheet.service.ts
@@ -1,33 +1,35 @@
 import { ConfigService } from './config.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, shareReplay, switchMap } from 'rxjs';
+import { map, Observable, of, shareReplay, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardGSheetService {
-  private url$: Observable<string>;
+  private baseUrl: string;
   private cacheMap: { [sprintId: string]: Observable<{ [id: string]: number }> } = {};
 
   constructor(private configService: ConfigService, private http: HttpClient) {
-    this.url$ = this.configService.loadByKeys<{ dashboardGSheetUrl: string }>(['dashboardGSheetUrl']).pipe(
-      map((cfg) => cfg.dashboardGSheetUrl ?? ''),
-      shareReplay(1),
-    );
+    this.configService
+      .loadByKeys<{ dashboardGSheetUrl: string }>(['dashboardGSheetUrl'])
+      .subscribe((cfg) => (this.baseUrl = cfg.dashboardGSheetUrl ?? ''));
   }
 
-  isSetGSheetUrl(): Observable<boolean> {
-    return this.url$.pipe(map((url) => !!url));
+  isSetGSheetUrl(): boolean {
+    return !!this.baseUrl;
   }
 
-  getUserPlanPointsMapBySprint(sprintId: number): Observable<{ [id: string]: number }> {
-    if (!(sprintId in this.cacheMap)) {
+  getUserPlanPointsMapBySprint(sprintId: number, useCache = true): Observable<{ [id: string]: number }> {
+    if (!this.baseUrl) {
+      console.log('Please set `dashboardGSheetUrl` first to use `DashboardGSheetService`');
+      return of({});
+    }
+    if (!(sprintId in this.cacheMap) || !useCache) {
       const postData = { func_name: 'get_user_plan_points_by_sprint', params: { sprint_id: sprintId } };
-      this.cacheMap[sprintId] = this.url$.pipe(
-        switchMap((url) => this.http.post<{ [id: string]: number }>(`${url}/query`, postData)),
-        shareReplay(1),
-      );
+      this.cacheMap[sprintId] = this.http
+        .post<{ [id: string]: number }>(`${this.baseUrl}/query`, postData)
+        .pipe(shareReplay(1));
     }
     return this.cacheMap[sprintId];
   }
