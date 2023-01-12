e8b098e6879db8f38f0cab6deaba1bd20c80f403
Jim <jim@koding.work>
2023-01-12 20:35:26 +0800

    refactor: remove not used log

--- projects/content-panel/src/app/services/config.service.ts
+++ projects/content-panel/src/app/services/config.service.ts
@@ -11,10 +11,8 @@ export class ConfigService {
   loadByKeys<T>(keys: string[]): Observable<T> {
     return new Observable<any>((subscriber) => {
       const EXTENSION_ID = getExtensionId();
-      console.log('send message', EXTENSION_ID);
 
       chrome.runtime.sendMessage(EXTENSION_ID, { chromeApi: 'storage.sync.get', data: { keys } }, (items) => {
-        console.log('get response', items);
         this.zone.run(() => {
           subscriber.next(items);
           subscriber.complete();
